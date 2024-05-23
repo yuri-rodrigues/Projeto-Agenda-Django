0f977528059f0918e4ec16fea23ac6ec331d988c
Yuri Rodrigues <yuri.alvess@hotmail.com>
2024-05-23 18:06:39 -0300

    Primeiras models criadas

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
-
+from contact import models
 # Register your models here.
+
+@admin.register(models.Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = 'first_name', 'last_name', 'phone', 'category'
+    ordering = '-id',
+    search_fields = 'id', 'first_name', 'last_name'
+    list_per_page = 10
+    list_max_show_all = 200
+    
+
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = 'name',
+    ordering = '-id',
