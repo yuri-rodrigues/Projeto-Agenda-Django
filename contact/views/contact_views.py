fce3ba50e69996a3fda0a2ef2734b65ca06cdedb
Yuri Rodrigues <yuri.alvess@hotmail.com>
2024-05-24 12:24:59 -0300

    Adicao de filtro para puxar pelo nome ou sobrenome

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from contact.models import Contact
+from django.db.models import Q
 
 # Create your views here.
 
@@ -10,7 +11,8 @@ def index(request):
     
 
     context = {
-        'contacts': contacts
+        'contacts': contacts,
+        'site_title': 'Contatos'
     }
 
     return render(
@@ -21,15 +23,45 @@ def index(request):
 
 def contact(request, contact_id):
     # single_contact = 
-    single_contact = get_object_or_404(Contact.objects.filter(pk=contact_id, show=True))
-    
+    single_contact = get_object_or_404(Contact.objects.filter(pk=contact_id, show=True))    
+    contact_name = f'{single_contact.first_name} {single_contact.last_name}'
     
     context = {
-        'contact': single_contact
+        'contact': single_contact,
+        'site_title': contact_name,
     }
 
     return render(
         request,
         'contact/contact.html',
+        context,
+    )
+
+
+def search(request):
+    search_value = request.GET.get('q', '').strip()
+
+    if search_value == '':
+        return redirect('contact:index')  
+
+
+    contacts = Contact.objects \
+        .filter(show=True)\
+        .filter(
+                Q(first_name__icontains=search_value) |
+                Q(last_name__icontains=search_value)
+                ) \
+        .order_by('-id')
+    
+    print(contacts.query)
+
+    context = {      
+        'contacts': contacts,
+        'site_title': search_value,
+    }
+
+    return render(
+        request,
+        'contact/index.html',
         context,
     )
